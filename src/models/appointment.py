048c48925c76c4e785fc7f16c995fcffe32f5d2f
HarshithaP25 <prakash9741341@gmail.com>
2026-02-04 14:54:16 +0530

    new

--- src/models/appointment.py
+++ src/models/appointment.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from datetime import datetime, timezone
-from src.database import Base
+from database import Base
 
 
 class Appointment(Base):
--- src/models/doctor.py
+++ src/models/doctor.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime, timezone
-from src.database import Base
+from database import Base
 
 
 class Doctor(Base):
